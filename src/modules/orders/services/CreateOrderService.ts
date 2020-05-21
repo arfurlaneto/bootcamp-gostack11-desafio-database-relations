3a6d8dfb279f681517a98afe4ccf23ff810466c5
arfurlaneto <arfurlaneto@gmail.com>
2020-05-20 21:50:22 -0300

    Controllers, services and repositories.

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,62 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
-  public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+  public async execute({
+    customer_id,
+    products: requestedProducts,
+  }: IRequest): Promise<Order> {
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Invalid customer.');
+    }
+
+    const stockProducts = await this.productsRepository.findAllById(
+      requestedProducts,
+    );
+
+    const orderProducts = requestedProducts.map(requestedProduct => {
+      const stockProduct = stockProducts.find(
+        p => p.id === requestedProduct.id,
+      );
+
+      if (!stockProduct) {
+        throw new AppError('Invalid product.');
+      }
+
+      if (stockProduct.quantity < requestedProduct.quantity) {
+        throw new AppError(
+          `Not enough items of "${stockProduct.name}" in stock.`,
+        );
+      }
+
+      stockProduct.quantity -= requestedProduct.quantity;
+
+      return {
+        product_id: stockProduct.id,
+        price: stockProduct.price,
+        quantity: requestedProduct.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    await this.productsRepository.updateQuantity(stockProducts);
+
+    return order;
   }
 }
 
